f5668d888eb32b52078c5f7854b3d3f6fa7d35e8
Mark William Fernandez <57150058+P0tat0S@users.noreply.github.com>
2021-11-04 17:29:26 +0000

    True Final Commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,19 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue;
     public float speed;
+    private int count;
+    private int numPickups = 24;
+    public Text scoreText;
+    public Text winText;
+
+    void Start() {
+        count = 0;
+        winText.text = "";
+        SetCountText();
+    }
 
     void OnMove(InputValue value) {
         moveValue = value.Get<Vector2>();
     }
 
-    void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(moveValue.x , 0.0f , moveValue.y);
-        GetComponent <Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+    void FixedUpdate(){
+        Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "PickUp") {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText() {
+        scoreText.text = "Score: " + count.ToString();
+        if(count >= numPickups) {
+            winText.text = "You win!";
+        }
     }
 }
